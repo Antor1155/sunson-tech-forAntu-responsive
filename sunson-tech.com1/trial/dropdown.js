88e0b7dd1094af1b91123330f17c2f0bc3bbc6f7
Md Antor <md.antor1155@gmail.com>
2022-08-12 09:01:18 +0600

    brought the nav bar menu to a good position :

--- sunson-tech.com1/trial/dropdown.js
+++ sunson-tech.com1/trial/dropdown.js
@@ -5,17 +5,46 @@ document.addEventListener('click', e =>{
     let currentDropdown
     if(isDropdownButton) {
         currentDropdown = e.target.closest("[data-dropdown]")
-        currentDropdown.classList.toggle("active")
+        currentDropdown.classList.toggle("on")
     }
 
+})
 
+document.addEventListener("click", e=>{
     // for sub menus the dropdon comes here 
-    // const isOption = e.target.matches("[data-option-button]")
-    // if(!isOption && e.target.closest('[data-option]') != null) return
+    const isOptionButton  = e.target.matches("[data-option-button]")
+    if(!isOptionButton && e.target.closest("[data-option]") !=null) return
 
-    // let curretnOption
-    // if(isOption) {
-    //     curretnOption = e.target.closest('[data-option]')
-    // }
+    let currentOption
+    if(isOptionButton) {
+        currentOption = e.target.closest("[data-option]")
+        currentOption.classList.toggle("active")
+    }
 
+    document.querySelectorAll(".allOption.active").forEach(option =>{
+        if(option === currentOption) return
+        option.classList.remove("active")
+    })
+})
+
+document.addEventListener('click', e=>{
+    let elements = document.querySelectorAll(".active")
+    console.log(elements)
+    if(elements.length >0){
+        document.querySelectorAll(".allOption").forEach(element =>{
+            console.log(element)
+            if(!element.classList.contains('active')){
+                element.style.display ="none";
+            }
+        })
+    }
+    else{
+        document.querySelectorAll(".allOption").forEach(element =>{
+            console.log(element)
+            if(!element.classList.contains('active')){
+                element.style.display ="block";
+            }
+        })
+
+    }
 })
