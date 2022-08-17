d5b7aaef304f87a5736c41690be4ccebec4b375e
Md Antor <md.antor1155@gmail.com>
2022-08-17 17:46:49 +0600

    industries and products section modified with sub menues

--- sunson-tech.com1/test2/a.js
+++ sunson-tech.com1/test2/a.js
@@ -120,23 +120,33 @@ document.addEventListener("click", e => {
             document.getElementById("scroll-option").style.gridTemplateColumns = '3fr 8fr ';
 
             document.getElementById("nav-scorelled-section").innerHTML = `
-            <button>Bank</button>
-            <button>Healthcare</button>
-            <button>Security Informaiton</button>
+            <button id="indBank">Bank</button>
+            <button id="indHealth">Healthcare</button>
+            <button id="indSecurity">Security</button>
+            <button id="indRetails">Retails and Products</button>
         `;
+
+            document.getElementById('nav-scorelled-sub-section').innerHTML = `
+                <div>
+
+                <div class="company-section">
+                <img src="../images/underConstruction.webp" alt="">
+                <button>ATM</button>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+                <a href="notfound.html"> Learn more > </a>
+                </div>
+
+                <div class="company-section">
+                <img src="../images/underConstruction.webp" alt="">
+                <button>CDM</button>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+                <a href="notfound.html"> Learn more > </a>
+                </div>
+
+                </div>
+                `
         }
 
-        else if (e.target.matches("#products")) {
-            document.getElementById("search").src = "../images/searchIcon.svg";
-
-            document.getElementById("scroll-option").style.gridTemplateColumns = '3fr 8fr ';
-
-            document.getElementById("nav-scorelled-section").innerHTML = `
-            <button>ATM/CDM/CRS</button>
-            <button>Healthcare Products</button>
-            <button>PIN PAD</button>
-        `;
-        }
 
         else if (e.target.matches("#company")) {
             document.getElementById("search").src = "../images/searchIcon.svg";
@@ -200,17 +210,17 @@ document.addEventListener("click", e => {
             // this section is for the cross mark when the search menu comes forward 
             if (srcName.includes('/images/cross-svgrepo-com.svg')) {
                 document.getElementById("search").src = "../images/searchIcon.svg";
-        
+
                 document.getElementById("scroll-option").classList.remove("down");
             }
 
-            else{
+            else {
 
                 document.getElementById("scroll-option").style.gridTemplateColumns = '1fr';
                 document.getElementById("nav-scorelled-sub-section").style.display = 'none';
-                
+
                 document.getElementById("search").src = "../images/cross-svgrepo-com.svg";
-                
+
                 document.getElementById("nav-scorelled-section").innerHTML = ` 
                 <div id='searchN'>
                 <input type="text" placeholder="search" style="width: 100%; ">
@@ -233,7 +243,7 @@ document.addEventListener("click", e => {
 
 
 
-
+// for solutions sections 
 
 document.addEventListener("click", e => {
 
@@ -292,6 +302,111 @@ document.addEventListener("click", e => {
         <a href="notfound.html">Hardware</a>
         <a href="notfound.html">Software</a>
         <a href="notfound.html">Information Security</a>`
+    }
+})
+
+
+
+// for industries section 
+document.addEventListener("click", e=>{
+    if (e.target.matches('#indBank')) {
+        document.getElementById('nav-scorelled-sub-section').innerHTML = `
+        <div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>ATM</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>CDM</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        </div>
+        `
+    }
+
+    if (e.target.matches('#indHealth')) {
+        document.getElementById('nav-scorelled-sub-section').innerHTML = `
+        <div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>Health Kiosk</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>Multi Parameter</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        </div>
+        `
+    }
+    if (e.target.matches('#indSecurity')) {
+        document.getElementById('nav-scorelled-sub-section').innerHTML = `
+        <div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>PCC</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>Metal Pinpad</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>Auto Kayboard</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        </div>
+        `
+    }
+    if (e.target.matches('#indRetails')) {
+        document.getElementById('nav-scorelled-sub-section').innerHTML = `
+        <div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>Digital Sinaque</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>Payroll</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        <div class="company-section">
+        <img src="../images/underConstruction.webp" alt="">
+        <button>Bitcoin</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laborum placeat enim temporibus, nobis necessitatibus. Molestiae ullam animi eaque sit?</p>
+        <a href="notfound.html"> Learn more > </a>
+        </div>
+
+        </div>
+        `
     }
 })
 
